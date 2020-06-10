6e67bee308ebf4f09bc7bcb89134802dda708c44
MM <marcinvxy@gmail.com>
2020-06-10 14:01:04 +0200

    change Main2

--- Main2.java
+++ Main2.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main2 {
 
     public static void main(String[] args) {
+
         System.out.println("Hello Main2");
+        System.out.println("Hello World Main2!");
+    
     }
 }
