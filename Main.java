916cbc2f8dbad5608b011bc9b0026deef8a97e88
MM <marcinvxy@gmail.com>
2020-06-10 13:54:51 +0200

    change Main

--- Main.java
+++ Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Hello Zarządzanie Zespołami i Projektami!!!");
+        System.out.println("Hello World!");
+
     }
 }
